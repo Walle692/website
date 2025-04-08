5baf5a02de629cc548f70fe78dcc169c7cdfa561
Walle692 <146271763+Walle692@users.noreply.github.com>
2025-04-08 11:28:32 +0200

    added navbar components

---
+++ src/app/components/NavBar/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import {nav, navbar, bars, navmenu, navlink, navbtn, navbtnlink} from "./navbar.module.css";
+
+const Navbar = () => {
+    return (
+        <>
+            <nav>
+                <bars />
+
+                <navmenu>
+                    <navlink href="/">Home</navlink>
+                    <navlink href="/services">Services</navlink>
+                    <navlink href="/contact">Contact</navlink>
+                </navmenu>
+            </nav>
+        </>
+    );
+};
+
+export default Navbar;
