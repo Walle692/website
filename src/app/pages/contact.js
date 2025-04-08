14f9ecf6a1141a8a363b106f81212965763f15a2
Walle692 <146271763+Walle692@users.noreply.github.com>
2025-04-08 12:18:15 +0200

    Added pages

---
+++ src/app/pages/contact.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Contact = () => {
+    return (
+        <div style = {{display: "flex",
+                       justifyContet: "center",
+                       alignItems: "center",
+                       height: "100vh",
+                    }}>
+            <h1>
+                This is our contact page, hope you find what you need.
+            </h1>
+        </div>
+    )
+}
+
+export default Contact;
---
+++ src/app/pages/services.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Services = () => {
+    return (
+        <div style = {{display: "flex",
+                       justifyContet: "center",
+                       alignItems: "center",
+                       height: "100vh",
+                    }}>
+            <h1>
+                This is our services page, hope you find what you need.
+            </h1>
+        </div>
+    )
+}
+
+export default Services;
