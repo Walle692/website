c3dd26f9b22057bd4c9b827c6d988e2611233858
Walle692 <146271763+Walle692@users.noreply.github.com>
2025-04-09 10:02:15 +0200

    tatatat

--- src/app/components/NavBar/Navbar.jsx
+++ src/app/components/NavBar/Navbar.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import PropTypes from 'prop-types'; // Import PropTypes
+import PropTypes from 'prop-types';
 import {
   Collapse,
   Navbar,
@@ -7,15 +7,12 @@ import {
   NavbarBrand,
   Nav,
   NavItem,
-  NavLink,
-  UncontrolledDropdown,
-  DropdownToggle,
-  DropdownMenu,
-  DropdownItem
+  NavLink
 } from 'reactstrap';
+import 'bootstrap/dist/css/bootstrap.min.css'; // Ensure Bootstrap CSS is imported
 import './NavbarStyle.module.css';
 
-export default class myNavbar extends React.Component {
+export default class MyNavbar extends React.Component {
   constructor(props) {
     super(props);
 
@@ -24,38 +21,39 @@ export default class myNavbar extends React.Component {
       isOpen: false
     };
   }
+
   toggle() {
     this.setState({
       isOpen: !this.state.isOpen
     });
   }
+
   render() {
     return (
-        <div className="navbar-container">
-            <Navbar className="navbar" expand="lg" container="fluid">
-                <NavbarBrand className="navbar-brand" href="/">HouseIndex</NavbarBrand>
-                <NavbarToggler className="navbar-toggler" onClick={this.toggle} />
-                <Collapse isOpen={this.state.isOpen} navbar>
-                    <Nav className="ml-auto" expand="ml-auto" navbar>
-                        <NavItem>
-                            <NavLink className="nav-link" href="/">Home</NavLink>
-                        </NavItem>
-                        <NavItem>
-                            <NavLink className="nav-link" href="/services">Services</NavLink>
-                        </NavItem>
-                        <NavItem>
-                            <NavLink className="nav-link" href="/contact">Contact</NavLink>
-                        </NavItem>
-                    </Nav>
-                </Collapse>
-            </Navbar>
-        </div>
+      <div className="navbar-container">
+        <Navbar color="light" light expand="lg" className="navbar">
+          <NavbarBrand href="/">HouseIndex</NavbarBrand>
+          <NavbarToggler onClick={this.toggle} />
+          <Collapse isOpen={this.state.isOpen} navbar>
+            <Nav className="ms-auto" navbar>
+              <NavItem>
+                <NavLink href="/">Home</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="/services">Services</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="/contact">Contact</NavLink>
+              </NavItem>
+            </Nav>
+          </Collapse>
+        </Navbar>
+      </div>
     );
   }
 }
 
-// Define propTypes for the Navbar component
-myNavbar.propTypes = {
+MyNavbar.propTypes = {
   light: PropTypes.bool,
   dark: PropTypes.bool,
   fixed: PropTypes.string,
